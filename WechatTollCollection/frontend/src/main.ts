86582dee91c622273bbc973bfdb4e6cc2cac5197
wltiger <77831808@qq.com>
2017-05-14 22:54:05 +0800

    [jiaxing][frontend] try to add Vux Grid to project, add i18n support.

--- WechatTollCollection/frontend/src/main.ts
+++ WechatTollCollection/frontend/src/main.ts
@@ -1,23 +1,50 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { PluginObject } from 'vue'
+import Vuex from 'vuex'
+
+
+import Es6Promise from 'es6-promise'
+Es6Promise.polyfill();
+
+
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+FastClick.attach(document.body)
+
+//i18n initialize
+import vuexI18n from 'vuex-i18n'
+Vue.use(<PluginObject<object>><any>Vuex);
+
+// initialize the vuex store using the vuex module. note that you can change the 
+//  name of the module if you wish 
+const store = new Vuex.Store({});
+ 
+// initialize the internationalization plugin on the vue instance. note that 
+// the store must be passed to the plugin. the plugin will then generate some 
+// helper functions for components (i.e. this.$i18n.set, this.$t) and on the vue 
+// instance (i.e. Vue.i18n.set). 
+Vue.use(vuexI18n.plugin, store);
+
+
 import App from './App.vue'
 import Home from './components/HelloFromVux.vue'
-
+import Grid from './components/Grid.vue'
+import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/',
   component: Home
+},{
+  path: '/Grid',
+  component: Grid
 }]
 
 const router = new VueRouter({
   routes
 })
 
-FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
